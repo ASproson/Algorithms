1021f4eb3b0324cf55a6909fa9499d20f8fdef49
atlas <a.sproson@outlook.com>
2022-11-27 10:00:00 +0000

    Solution

--- Easy/E_otherAngle.js
+++ Easy/E_otherAngle.js
@@ -1,7 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.otherAngle = void 0;
-const otherAngle = (a, b) => {
-    return 180 - (a + b);
-};
+const otherAngle = (a, b) => 180 - (a + b);
 exports.otherAngle = otherAngle;
