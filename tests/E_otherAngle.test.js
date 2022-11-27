5a2eb5cde961ee6ce6f5e7f2a455f68585be1c42
atlas <a.sproson@outlook.com>
2022-11-27 10:00:00 +0000

    tsc build

---
+++ tests/E_otherAngle.test.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const E_otherAngle_1 = require("../Easy/E_otherAngle");
+E_otherAngle_1.otherAngle;
+describe('otherAngle', () => {
+    it('Sample Tests', () => {
+        expect((0, E_otherAngle_1.otherAngle)(30, 60)).toBe(90);
+        expect((0, E_otherAngle_1.otherAngle)(60, 60)).toBe(60);
+        expect((0, E_otherAngle_1.otherAngle)(43, 78)).toBe(59);
+        expect((0, E_otherAngle_1.otherAngle)(10, 20)).toBe(150);
+    });
+});
