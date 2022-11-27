c2d887505e11ee592a0ee0455f77669758339185
atlas <a.sproson@outlook.com>
2022-11-27 10:00:00 +0000

    Tsc build

---
+++ Easy/E_otherAngle.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.otherAngle = void 0;
+const otherAngle = (a, b) => {
+    return 180 - (a + b);
+};
+exports.otherAngle = otherAngle;
