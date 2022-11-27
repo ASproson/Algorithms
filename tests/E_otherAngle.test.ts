e2b10d181a15d735af6b8f58f9290d2cd9bd3404
atlas <a.sproson@outlook.com>
2022-11-27 10:00:00 +0000

    Unit test

---
+++ tests/E_otherAngle.test.ts
@@ -0,0 +1,12 @@
+import { otherAngle } from '../Easy/E_otherAngle';
+
+otherAngle;
+
+describe('otherAngle', () => {
+  it('Sample Tests', () => {
+    expect(otherAngle(30, 60)).toBe(90);
+    expect(otherAngle(60, 60)).toBe(60);
+    expect(otherAngle(43, 78)).toBe(59);
+    expect(otherAngle(10, 20)).toBe(150);
+  });
+});
