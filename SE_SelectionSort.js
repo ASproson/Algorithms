ef0566646beb85720db666c06ffd7a3cbbf345c5
atlas <a.sproson@outlook.com>
2022-12-03 10:43:33 +0000

    build

--- SE_SelectionSort.js
+++ SE_SelectionSort.js
@@ -1,19 +1,19 @@
 "use strict";
 const selectionSort = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        if (minIndex !== i) {
-            let temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
     }
-    return arr;
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
 };
 module.exports = { selectionSort };
 // Space: O(1) as we sort the array in place
