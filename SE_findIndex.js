60e1891d5c67fa7d46f1df1dc247a1002a7f6b7c
atlas <a.sproson@outlook.com>
2022-12-24 10:42:31 +0000

    build

--- SE_findIndex.js
+++ SE_findIndex.js
@@ -7,5 +7,6 @@ module.exports = { findIndex };
 
 // Time: O(n) as we potentially traverse entire array
 // Space: O(1) as only using provided values
+
 // Alternatives: binary search(O log n), however, would need a sorted array
 // Alternatives: store values and their indexes in a map to look up index value in O(1) time, but will take O(n) space
