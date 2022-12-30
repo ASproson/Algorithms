305fb8ea4b3e576fa73d0d2cd31dfe3faaf801ae
atlas <a.sproson@outlook.com>
2022-12-30 10:40:36 +0000

    build

--- SE_add.js
+++ SE_add.js
@@ -2,6 +2,7 @@
 const add = (a, b) => {
   return a + b;
 };
+
 module.exports = add;
 // Space complexity is O(1) as the func only requires a fixed amount of space to store a few primitive values (a, b, and their sum)
 // Time complexity is O(1) as the function executes in the same of time regardless of the size of the input, i.e (1000, 10000000) is no different from (1 + 1)
