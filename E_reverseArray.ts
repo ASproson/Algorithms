5e9770d3b927163fe5fa8df6c538b5dc40132289
atlas <a.sproson@outlook.com>
2023-01-06 15:42:28 +0000

    Adds prompt

--- E_reverseArray.ts
+++ E_reverseArray.ts
@@ -1,3 +1,5 @@
+// Write a function called reverseArray that takes in an array of integers and returns a new array with the integers in reverse order. The function should use pointers to reverse the array in place, meaning that it should not create a new array to hold the reversed elements
+
 const reverseArray = (arr: number[]): number[] => {
   let left = 0;
   let right = arr.length - 1;
@@ -15,3 +17,6 @@ const reverseArray = (arr: number[]): number[] => {
 };
 
 module.exports = { reverseArray };
+
+// Time: O(n) as we have to interact with the entire array
+// Space: O(1) as we reverse the array in place
