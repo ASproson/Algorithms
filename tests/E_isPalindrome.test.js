80d4655965933af2c114f49135a917a6be442ab0
atlas <a.sproson@outlook.com>
2023-01-06 18:32:17 +0000

    Adds E_isPalindrome

---
+++ tests/E_isPalindrome.test.js
@@ -0,0 +1,19 @@
+const { isPalindrome } = require("../E_isPalindrome");
+
+describe("isPalindrome()", () => {
+  it("returns a boolean", () => {
+    expect(typeof isPalindrome("racecar")).toEqual("boolean");
+  }),
+    it("returns true when passed racecar", () => {
+      expect(isPalindrome("racecar")).toEqual(true);
+    }),
+    it("returns true when passed A man, a plan, a canal, Panama!", () => {
+      expect(isPalindrome("A man, a plan, a canal, Panama!")).toEqual(true);
+    }),
+    it("returns true when passed Never odd or even.", () => {
+      expect(isPalindrome("Never odd or even.")).toEqual(true);
+    }),
+    it("returns false when passed FALSE.", () => {
+      expect(isPalindrome("FALSE")).toEqual(false);
+    });
+});
