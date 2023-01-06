47a47f6ccfd467a627396003dd79a4aee6453d78
atlas <a.sproson@outlook.com>
2023-01-06 11:49:36 +0000

    Adds E_pivotIndex

---
+++ E_pivotIndex.ts
@@ -0,0 +1,40 @@
+const pivotIndex = (nums: number[]): number => {
+  const sum = nums.reduce((acc, cur) => acc + cur, 0);
+
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSum === sum - leftSum - nums[i]) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+
+  return -1;
+};
+
+module.exports = { pivotIndex };
+
+// const pivotIndex = (nums: number[]): number => {
+//   let leftSum = 0;
+//   let rightSum = 0;
+
+//   for (let i = 1; i < nums.length; i++) {
+//     rightSum += nums[i];
+//   }
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (leftSum === rightSum) {
+//       return i;
+//     }
+//     leftSum += nums[i];
+//     rightSum -= nums[i + 1] || 0;
+//   }
+
+//   return -1;
+// };
+
+// module.exports = { pivotIndex };
+
+// Time: O(n) as we iterate through the array once for rightSum, and a second time for the pivotIndex
+// Space: O(1) as it uses a constant amount of memory regardless of input array size
---
+++ tests/E_pivotIndex.test.js
@@ -0,0 +1,16 @@
+const { pivotIndex } = require("../E_pivotIndex");
+
+describe("pivotIndex", () => {
+  it("returns type number", () => {
+    expect(typeof pivotIndex([1, 2, 3])).toBe("number");
+  }),
+    it("returns i of 3 when passed [1, 7, 3, 6, 5, 6]", () => {
+      expect(pivotIndex([1, 7, 3, 6, 5, 6])).toEqual(3);
+    }),
+    it("returns -1 when passed [1, 2, 3]", () => {
+      expect(pivotIndex([1, 2, 3])).toEqual(-1);
+    }),
+    it("returns i of 0 when passed [2, 1, -1]", () => {
+      expect(pivotIndex([2, 1, -1])).toEqual(0);
+    });
+});
