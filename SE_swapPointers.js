93ca45af12959b42da5f0c94eda174dfdf1d16cc
atlas <a.sproson@outlook.com>
2023-01-09 10:53:32 +0000

    Removes logs

--- SE_swapPointers.js
+++ SE_swapPointers.js
@@ -5,8 +5,4 @@ const swapPointers = (a, b) => {
     b = temp;
     return [a, b];
 };
-let a = 5;
-let b = "hello";
-let [x, y] = swapPointers(a, b);
-console.log(x, y);
 module.exports = { swapPointers };
