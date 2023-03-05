274bf4a407b615f52a816e522830c34ec6c693ea
atlas <a.sproson@outlook.com>
2023-03-05 10:28:20 +0000

    build

--- E_MiddleOfLinkedList.js
+++ E_MiddleOfLinkedList.js
@@ -1,24 +1,24 @@
 "use strict";
 class LinkNode {
-    val;
-    next;
-    constructor(val, next) {
-        this.val = val === undefined ? 0 : val;
-        this.next = next === undefined ? null : next;
-    }
+  val;
+  next;
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 // Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node
 const middleNode = (head) => {
-    if (!head) {
-        return null;
-    }
-    let slow = head;
-    let fast = head;
-    while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-    return slow;
+  if (!head) {
+    return null;
+  }
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
 };
 module.exports = { middleNode, LinkNode };
 // Time: O(n) as we need to touch every node
