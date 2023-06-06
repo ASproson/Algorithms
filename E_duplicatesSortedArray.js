e223203171fc954c7e4ce6bfd2bb308650304fc0
atlas <a.sproson@outlook.com>
2023-06-06 19:20:43 +0100

    Adds duplicaesSortedArray

---
+++ E_duplicatesSortedArray.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.removeDuplicatesSortedArray = void 0;
+const removeDuplicatesSortedArray = (nums) => {
+    if (!nums)
+        return 0;
+    let uniqueValueCounter = 1;
+    let heldValue = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== heldValue) {
+            uniqueValueCounter++;
+            heldValue = nums[i];
+            nums[uniqueValueCounter - 1] = heldValue;
+        }
+    }
+    return uniqueValueCounter;
+};
+exports.removeDuplicatesSortedArray = removeDuplicatesSortedArray;
