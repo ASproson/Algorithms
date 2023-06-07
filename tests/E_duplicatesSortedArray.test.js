e52923e3dc9f1bcdc5e5041c23db237e1093d93e
atlas <a.sproson@outlook.com>
2023-06-07 08:02:07 +0100

    Adds assertion for modifying the array in place

--- tests/E_duplicatesSortedArray.test.js
+++ tests/E_duplicatesSortedArray.test.js
@@ -4,13 +4,14 @@ describe("removeDuplicatesSortedArray()", () => {
   test("returns 0 when given nothing", () => {
     expect(removeDuplicatesSortedArray()).toEqual(0);
   });
-  test("returns [1, 2] when passed [1, 1, 2]", () => {
+  test("returns [1, 2] when passed [1, 1, 2] and modifies the original array", () => {
     let nums = [1, 1, 2];
     expect(removeDuplicatesSortedArray(nums)).toEqual(2);
-    // expect(nums).toEqual([1, 2]);
+    expect(nums.slice(0, 2)).toEqual([1, 2]);
   });
   test("returns [1,2,3,4] when passed [0,0,1,1,1,2,2,3,3,4]", () => {
     const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
     expect(removeDuplicatesSortedArray(nums)).toEqual(5);
+    expect(nums.slice(0, 5)).toEqual([0, 1, 2, 3, 4]);
   });
 });
