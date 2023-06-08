7a1d1125c51c5ac28ec71559b72f6b27eee36814
atlas <a.sproson@outlook.com>
2023-06-08 08:53:40 +0100

    Adds BigO notation

--- E_removeElement.ts
+++ E_removeElement.ts
@@ -7,3 +7,6 @@ export const removeElement = (nums: number[], val: number): number => {
   }
   return nums.length;
 };
+
+// Time complexity is O(n) as we have to traverse the entire array to ensure all the deletions occur
+// Space complexity is O(1) as it uses a constant amount of space as modify the array in place
