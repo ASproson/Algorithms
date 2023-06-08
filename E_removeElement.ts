c137a8d06ba8f6d5ba5e49937582cedb137ef7ad
atlas <a.sproson@outlook.com>
2023-06-08 08:51:23 +0100

    Resolves function

--- E_removeElement.ts
+++ E_removeElement.ts
@@ -1,1 +1,9 @@
-export const removeElement = () => {};
+export const removeElement = (nums: number[], val: number): number => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
