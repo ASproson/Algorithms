b5bde853da5a283d941a7cd1ab7d4f452db48030
atlas <a.sproson@outlook.com>
2023-06-08 08:51:27 +0100

    tsc build

--- E_removeElement.js
+++ E_removeElement.js
@@ -1,5 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.removeElement = void 0;
-const removeElement = () => { };
+const removeElement = (nums, val) => {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+};
 exports.removeElement = removeElement;
