89c4ccc218e45e99fb31a087ad5ecb82ae69bf77
atlas <a.sproson@outlook.com>
2023-06-08 08:12:30 +0100

    Initial test

---
+++ tests/E_removeElement.test.js
@@ -0,0 +1,6 @@
+const { removeElement } = require("../E_removeElement");
+// const { removeDuplicates } = require("../E_removeDuplicates");
+
+describe("removeElement()", () => {
+  it("", () => {});
+});
