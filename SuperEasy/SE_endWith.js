97d0e58311be32c62e556d408adbfeea3b8a7ab5
atlas <a.sproson@outlook.com>
2023-06-18 12:00:00 +0100

    Solution

---
+++ SuperEasy/SE_endWith.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.endWith = void 0;
+const endWith = (str, ending) => {
+    return str.endsWith(ending);
+};
+exports.endWith = endWith;
