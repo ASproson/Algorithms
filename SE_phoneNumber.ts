c5b45a64bce81bf941afb15b60a0c071da6e6c8d
atlas <a.sproson@outlook.com>
2023-07-13 20:15:12 +0100

    Solution

---
+++ SE_phoneNumber.ts
@@ -0,0 +1,12 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number
+// phoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) returns "(123) 456-7890"
+
+const phoneNumber = (numbers: number[]): string => {
+  let str = "(";
+  str += numbers.slice(0, 3).join("") + ") ";
+  str += numbers.slice(3, 6).join("") + "-";
+  str += numbers.slice(6, 10).join("");
+  return str;
+};
+
+module.exports = { phoneNumber };
