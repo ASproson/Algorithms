16291392bc8cdfb72f6b6243335709af0b8a386b
atlas <a.sproson@outlook.com>
2023-07-13 17:57:15 +0100

    Adds comments

--- M_exponentialDigit.ts
+++ M_exponentialDigit.ts
@@ -11,3 +11,11 @@ const exponentialDigit = (n: number, p: number): number => {
 };
 
 module.exports = { exponentialDigit };
+
+// Split n into a string so we can map over that array
+// Store current value of array[i] to the power of p + i, add to sum
+// If sum % n has remainders, return -1 else return sum / 2
+// exponentialDigit(89, 1)
+// 8 ** p + i === 8 ** 0 ┤ 9 ** p + i === 9 ** 1
+// 8^1 + 9^2; sum = 8 + 81 === 89
+// 89 % 89 === 0, no remainders, k exists
