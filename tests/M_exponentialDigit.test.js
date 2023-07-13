8e4d3911f3f3f01d9555dcc0fd2af5c720929097
atlas <a.sproson@outlook.com>
2023-07-13 17:52:34 +0100

    Adds unit test

---
+++ tests/M_exponentialDigit.test.js
@@ -0,0 +1,13 @@
+const { exponentialDigit } = require("../M_exponentialDigit");
+
+describe("exponentialDigit", () => {
+  it("returns 1 when passed 89, 1", () => {
+    expect(exponentialDigit(89, 1)).toEqual(1);
+  });
+  it("returns -1 when passed 92, 1", () => {
+    expect(exponentialDigit(92, 1)).toEqual(-1);
+  });
+  it("returns 9 when passed 114, 3", () => {
+    expect(exponentialDigit(114, 3)).toEqual(9);
+  });
+});
