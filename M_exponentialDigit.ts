db192beb48a1c06e21c8dd74ac225c156250ede6
atlas <a.sproson@outlook.com>
2023-07-13 17:52:24 +0100

    Solution

---
+++ M_exponentialDigit.ts
@@ -0,0 +1,13 @@
+const exponentialDigit = (n: number, p: number): number => {
+  const array = String(n).split("");
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    let cur = Number(array[i]) ** (p + i);
+    sum += cur;
+  }
+
+  return sum % n ? -1 : sum / n;
+};
+
+module.exports = { exponentialDigit };
