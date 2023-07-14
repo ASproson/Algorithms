95ee3c3df3bf1febc05225c50b2fec26a2ed487c
atlas <a.sproson@outlook.com>
2023-07-14 18:28:15 +0100

    Solution

---
+++ M_longestConsecutiveString.ts
@@ -0,0 +1,19 @@
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array
+
+const longestConsecutiveString = (strarr: string[], k: number): string => {
+  if (k <= 0 || strarr.length === 0 || k > strarr.length) return "";
+
+  let longestString = "";
+
+  for (let i = 0; i <= strarr.length - k; i++) {
+    const currentString = strarr.slice(i, i + k).join("");
+
+    if (currentString.length > longestString.length) {
+      longestString = currentString;
+    }
+  }
+
+  return longestString;
+};
+
+module.exports = { longestConsecutiveString };
