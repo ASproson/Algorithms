7c8a51cafa51b1f28aeec197eb5bb82fe3c82dcf
atlas <a.sproson@outlook.com>
2023-07-15 17:21:06 +0100

    Unit test

---
+++ tests/E_pyramidArray.test.js
@@ -0,0 +1,16 @@
+const { pyramidArray } = require("../E_pyramidArray");
+
+describe("pyramidArray()", () => {
+  test("returns [] when given 0", () => {
+    expect(pyramidArray(0)).toEqual([]);
+  });
+  test("returns [[1]] when given 1", () => {
+    expect(pyramidArray(1)).toEqual([[1]]);
+  });
+  test("returns [[1], [1, 1]] when given 2", () => {
+    expect(pyramidArray(2)).toEqual([[1], [1, 1]]);
+  });
+  test("returns [[1], [1, 1], [1, 1, 1]] when given 3", () => {
+    expect(pyramidArray(3)).toEqual([[1], [1, 1], [1, 1, 1]]);
+  });
+});
