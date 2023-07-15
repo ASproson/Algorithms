61826ae5edbbbde0f7c86367d1e377ba438c837c
atlas <a.sproson@outlook.com>
2023-07-15 13:20:03 +0100

    Adds build

--- SE_findIndex.js
+++ SE_findIndex.js
@@ -1,12 +1,9 @@
 "use strict";
-
 const findIndex = (arr, num) => {
-  return arr.indexOf(num);
+    return arr.indexOf(num);
 };
 module.exports = { findIndex };
-
 // Time: O(n) as we potentially traverse entire array
 // Space: O(1) as only using provided values
-
 // Alternatives: binary search(O log n), however, would need a sorted array
 // Alternatives: store values and their indexes in a map to look up index value in O(1) time, but will take O(n) space
