20cdaa0f45ba6ee3b52b38e346acbd5cd027bbe9
atlas <a.sproson@outlook.com>
2023-07-15 13:19:20 +0100

    Adds unit test

---
+++ tests/E_deadFish.test.js
@@ -0,0 +1,10 @@
+const { deadFish } = require("../E_deadFish");
+
+describe("deadFish()", () => {
+  test('expect "iiisdoso" to be [8, 64', () => {
+    expect(deadFish("iiisdoso")).toEqual([8, 64]);
+  });
+  test('expect "iiisxxxdoso" to be [8, 64', () => {
+    expect(deadFish("iiisxxxdoso")).toEqual([8, 64]);
+  });
+});
