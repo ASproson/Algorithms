9c81da38f9465b9126d3eeabee8a5e406f720a26
atlas <a.sproson@outlook.com>
2023-07-15 13:19:44 +0100

    Adds build

--- E_bitCounting.js
+++ E_bitCounting.js
@@ -1,12 +1,10 @@
 "use strict";
 // Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
-
 const bitCounting = (n) => {
-  return n
-    .toString(2)
-    .split("")
-    .filter((num) => Number(num) === 1).length;
+    return n
+        .toString(2)
+        .split("")
+        .filter((num) => Number(num) === 1).length;
 };
-
 module.exports = { bitCounting };
