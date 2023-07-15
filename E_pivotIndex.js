4d7eb52a9cefdc7b715f77c962b8f64f90c7965f
atlas <a.sproson@outlook.com>
2023-07-15 13:19:55 +0100

    Adds build

--- E_pivotIndex.js
+++ E_pivotIndex.js
@@ -1,19 +1,16 @@
 "use strict";
-
 const pivotIndex = (nums) => {
-  const sum = nums.reduce((acc, cur) => acc + cur, 0);
-  let leftSum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (leftSum === sum - leftSum - nums[i]) {
-      return i;
+    const sum = nums.reduce((acc, cur) => acc + cur, 0);
+    let leftSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSum === sum - leftSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
     }
-    leftSum += nums[i];
-  }
-  return -1;
+    return -1;
 };
-
 module.exports = { pivotIndex };
-
 // const pivotIndex = (nums: number[]): number => {
 //   let leftSum = 0;
 //   let rightSum = 0;
