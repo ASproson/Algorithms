a5779369bb6f88ca7f0d86453285b23af3ee7f29
atlas <a.sproson@outlook.com>
2023-07-17 09:03:36 +0100

    Adds unit test

---
+++ tests/H_matrixDeterminant.test.js
@@ -0,0 +1,23 @@
+const { matrixDeterminant } = require("../H_matrixDeterminant");
+
+const m1 = [
+  [1, 3],
+  [2, 5],
+];
+const m2 = [
+  [2, 5, 3],
+  [1, -2, -1],
+  [1, 3, 4],
+];
+
+describe("matrixDeterminant()", () => {
+  test("when given [[1]] it returns 1", () => {
+    expect(matrixDeterminant([[1]])).toEqual(1);
+  });
+  test("when given m1 it returns -1", () => {
+    expect(matrixDeterminant(m1)).toEqual(-1);
+  });
+  test("when given m2 it returns -20", () => {
+    expect(matrixDeterminant(m2)).toEqual(-20);
+  });
+});
