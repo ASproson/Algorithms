3fbca29767db9908cb18c7b013d94ffec3649365
atlas <a.sproson@outlook.com>
2023-07-17 09:03:27 +0100

    Solution

---
+++ H_matrixDeterminant.ts
@@ -0,0 +1,30 @@
+// Write a function that accepts a square matrix (N x N 2D array) and returns the determinant of the matrix
+
+export const matrixDeterminant = (m: number[][]) => {
+  if (m.length === 1) {
+    return m[0][0];
+  } else if (m.length === 2) {
+    return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+  } else {
+    let sum = 0;
+    for (let j = 0; j < m.length; j++) {
+      const submatrix = [];
+      for (let row = 1; row < m.length; row++) {
+        const subrow = [];
+        for (let col = 0; col < m[row].length; col++) {
+          if (col !== j) {
+            subrow.push(m[row][col]);
+          }
+        }
+        submatrix.push(subrow);
+      }
+      const submatrixDet = matrixDeterminant(submatrix);
+      if (j % 2 === 0) {
+        sum += m[0][j] * submatrixDet;
+      } else {
+        sum -= m[0][j] * submatrixDet;
+      }
+    }
+    return sum;
+  }
+};
