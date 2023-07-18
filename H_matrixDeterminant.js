232ec28696ee763f5c42915b1c4e78d7870d7f6b
atlas <a.sproson@outlook.com>
2023-07-18 17:27:41 +0100

    tsc build

--- H_matrixDeterminant.js
+++ H_matrixDeterminant.js
@@ -3,30 +3,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.matrixDeterminant = void 0;
 const matrixDeterminant = (m) => {
-    if (m.length === 1)
+    if (m.length === 1) {
         return m[0][0];
-    if (m.length === 2)
-        return m[0][0] * m[1][1] - m[0][1] * m[1][0];
-    let sum = 0;
-    for (let j = 0; j < m.length; j++) {
-        const submatrix = [];
-        for (let row = 1; row < m.length; row++) {
-            const subrow = [];
-            for (let col = 0; col < m[row].length; col++) {
-                if (col !== j) {
-                    subrow.push(m[row][col]);
-                }
-            }
-            submatrix.push(subrow);
-        }
-        const submatrixDet = (0, exports.matrixDeterminant)(submatrix);
-        if (j % 2 === 0) {
-            sum += m[0][j] * submatrixDet;
-        }
-        else {
-            sum -= m[0][j] * submatrixDet;
-        }
     }
-    return sum;
+    else if (m.length === 2) {
+        return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+    }
+    else {
+        let sum = 0;
+        for (let j = 0; j < m.length; j++) {
+            const submatrix = [];
+            for (let row = 1; row < m.length; row++) {
+                const subrow = [];
+                for (let col = 0; col < m[row].length; col++) {
+                    if (col !== j) {
+                        subrow.push(m[row][col]);
+                    }
+                }
+                submatrix.push(subrow);
+            }
+            const submatrixDet = (0, exports.matrixDeterminant)(submatrix);
+            if (j % 2 === 0) {
+                sum += m[0][j] * submatrixDet;
+            }
+            else {
+                sum -= m[0][j] * submatrixDet;
+            }
+        }
+        return sum;
+    }
 };
 exports.matrixDeterminant = matrixDeterminant;
