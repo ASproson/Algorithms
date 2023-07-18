6c3812f414b14372dddb4bd433c4e781c9e18372
atlas <a.sproson@outlook.com>
2023-07-18 17:27:44 +0100

    tsc build

---
+++ SE_reverseString.js
@@ -0,0 +1,8 @@
+"use strict";
+// Reverse the string passed in: word >>> drow
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.reverseString = void 0;
+const reverseString = (str) => {
+    return str.split("").reverse().join("");
+};
+exports.reverseString = reverseString;
