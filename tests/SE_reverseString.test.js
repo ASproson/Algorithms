affab82358403c2096335d6558a9855e75d607ae
atlas <a.sproson@outlook.com>
2023-07-18 17:27:28 +0100

    Unit test

---
+++ tests/SE_reverseString.test.js
@@ -0,0 +1,10 @@
+const { reverseString } = require("../SE_reverseString");
+
+describe("reverseString()", () => {
+  test("returns drow when passed word", () => {
+    expect(reverseString("word")).toEqual("drow");
+  });
+  test("returns olleh when passed hello", () => {
+    expect(reverseString("hello")).toEqual("olleh");
+  });
+});
