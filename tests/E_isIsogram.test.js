7943a0fd33246fda367edba7538a8c523d2722ad
atlas <a.sproson@outlook.com>
2023-07-19 17:34:40 +0100

    Unit test

---
+++ tests/E_isIsogram.test.js
@@ -0,0 +1,23 @@
+const { isIsogram } = require("../E_isIsogram");
+
+describe("isIsogram()", () => {
+  test("returns true when given Dermatoglyphics", () => {
+    expect(isIsogram("Dermatoglyphics")).toEqual(true);
+  });
+  test("returns true when given isogram", () => {
+    expect(isIsogram("isogram")).toEqual(true);
+  });
+  test("returns false when given aba", () => {
+    expect(isIsogram("aba")).toEqual(false);
+  });
+  test("returns false when given moOse", () => {
+    expect(isIsogram("moOse")).toEqual(false);
+  });
+  test("returns false when given isIsogram", () => {
+    expect(isIsogram("isIsogram")).toEqual(false);
+  });
+
+  test("returns true when given empty string", () => {
+    expect(isIsogram("")).toEqual(true);
+  });
+});
