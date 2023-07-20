9e3c97ca6dc0ac67cd0a19fa84d81441821767da
atlas <a.sproson@outlook.com>
2023-07-20 17:48:23 +0100

    Unit test

---
+++ tests/SE_removeChar.test.ts
@@ -0,0 +1,16 @@
+const { removeChar } = require("../SE_removeChar");
+
+describe("removeChar()", () => {
+  test("when given eloquent return loquen", () => {
+    expect(removeChar("eloquent")).toEqual("loquen");
+  });
+  test("when given country return ountr", () => {
+    expect(removeChar("country")).toEqual("ountr");
+  });
+  test("when given person return erso", () => {
+    expect(removeChar("person")).toEqual("erso");
+  });
+  test("when given place return lac", () => {
+    expect(removeChar("place")).toEqual("lac");
+  });
+});
