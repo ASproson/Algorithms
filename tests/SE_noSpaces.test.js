32588c17b5805bb6982f2130be9b8027e6cb5b71
atlas <a.sproson@outlook.com>
2023-07-22 09:01:23 +0100

    tsc build

---
+++ tests/SE_noSpaces.test.js
@@ -0,0 +1,16 @@
+"use strict";
+const { noSpaces } = require("../SE_noSpaces");
+describe("noSpaces()", () => {
+    test("when given 8 j 8   mBliB8g  imjB8B8  jl  B expect 8j8mBliB8gimjB8B8jlB", () => {
+        expect(noSpaces("8 j 8   mBliB8g  imjB8B8  jl  B")).toEqual("8j8mBliB8gimjB8B8jlB");
+    });
+    test("when given 8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd returns 88Bifk8hB8BB8BBBB888chl8BhBfd", () => {
+        expect(noSpaces("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd")).toEqual("88Bifk8hB8BB8BBBB888chl8BhBfd");
+    });
+    test("when given 8aaaaa dddd r      returns 8aaaaaddddr", () => {
+        expect(noSpaces("8aaaaa dddd r     ")).toEqual("8aaaaaddddr");
+    });
+    test('when given "" returns ', () => {
+        expect(noSpaces("")).toEqual("");
+    });
+});
