09483a585f475d44c1ae9c6d1fe7ce4b71f3e789
atlas <a.sproson@outlook.com>
2023-07-22 09:01:20 +0100

    tsc build

---
+++ SE_noSpaces.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.noSpaces = void 0;
+const noSpaces = (str) => {
+    return str.split(" ").join("");
+};
+exports.noSpaces = noSpaces;
