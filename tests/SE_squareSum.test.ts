5d4bc198868431b233cba27c0df627bbbfe153a7
atlas <a.sproson@outlook.com>
2023-07-22 09:08:46 +0100

    Unit test

---
+++ tests/SE_squareSum.test.ts
@@ -0,0 +1,13 @@
+const { squareSum } = require("../SE_squareSum");
+
+describe("squareSum()", () => {
+  test("when given [1,2] returns 5", () => {
+    expect(squareSum([1, 2])).toEqual(5);
+  });
+  test("when given [0, 3, 4, 5] returns 50", () => {
+    expect(squareSum([0, 3, 4, 5])).toEqual(50);
+  });
+  test("when given [] returns 0", () => {
+    expect(squareSum([])).toEqual(0);
+  });
+});
