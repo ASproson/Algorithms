b68f8845f673b043f8e3ff308e8f612025d8d6eb
atlas <a.sproson@outlook.com>
2023-07-24 08:01:47 +0100

    tsc build

---
+++ tests/SE_greet.test.js
@@ -0,0 +1,10 @@
+"use strict";
+const { greet } = require("../SE_greet");
+describe("greet()", () => {
+    test("when given Ryan returns Hello, Ryan how are you doing today?", () => {
+        expect(greet("Ryan")).toEqual("Hello, Ryan how are you doing today?");
+    });
+    test("when given tnEtcPWlwpsynuhSpLByyCwqhlcN returns tnEtcPWlwpsynuhSpLByyCwqhlcN", () => {
+        expect(greet("tnEtcPWlwpsynuhSpLByyCwqhlcN")).toEqual("Hello, tnEtcPWlwpsynuhSpLByyCwqhlcN how are you doing today?");
+    });
+});
