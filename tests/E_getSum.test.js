e6398474007974acc5062ab1886e2087b027844f
atlas <a.sproson@outlook.com>
2023-07-27 15:54:18 +0100

    tsc build

---
+++ tests/E_getSum.test.js
@@ -0,0 +1,10 @@
+"use strict";
+const { getSum } = require("../E_getSum");
+describe("getSum()", () => {
+    test("when given 0,-1 it returns -1", () => {
+        expect(getSum(0, -1)).toEqual(-1);
+    });
+    test("when given 0,1 it returns 1", () => {
+        expect(getSum(0, 1)).toEqual(1);
+    });
+});
