f538b6fe7ee82ac8b37f252c73ca159247f7a6ee
atlas <a.sproson@outlook.com>
2023-07-29 18:20:00 +0100

    tsc build

---
+++ SE_paperwork.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.paperwork = void 0;
+const paperwork = (n, m) => {
+    return n <= 0 || m <= 0 ? 0 : n * m;
+};
+exports.paperwork = paperwork;
