2820e25f6c4e555505016d3c80d715540f42973e
atlas <a.sproson@outlook.com>
2023-07-29 18:20:13 +0100

    unit test

---
+++ tests/SE_paperwork.test.ts
@@ -0,0 +1,19 @@
+const { paperwork } = require("../SE_paperwork");
+
+describe("paperwork()", () => {
+  test("when given 5, 5 it returns 25", () => {
+    expect(paperwork(5, 5)).toEqual(25);
+  });
+  test("when given 5, -5 it returns 0", () => {
+    expect(paperwork(5, -5)).toEqual(0);
+  });
+  test("when given -5, -5 it returns 0", () => {
+    expect(paperwork(-5, -5)).toEqual(0);
+  });
+  test("when given -5, 5 it returns 0", () => {
+    expect(paperwork(-5, 5)).toEqual(0);
+  });
+  test("when given 5, 0 it returns 0", () => {
+    expect(paperwork(5, 0)).toEqual(0);
+  });
+});
