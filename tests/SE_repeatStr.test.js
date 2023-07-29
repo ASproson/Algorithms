e71ba59bc70ee3d34ca0c90792ae266a49275719
atlas <a.sproson@outlook.com>
2023-07-29 18:20:20 +0100

    unit test

--- tests/SE_repeatStr.test.js
+++ tests/SE_repeatStr.test.js
@@ -1,13 +1,13 @@
 "use strict";
 const { repeatStr } = require("../SE_repeatStr");
 describe("repeatStr()", () => {
-    test("when given 2, str it returns strstr", () => {
+    test("when given 2, str; it returns strstr", () => {
         expect(repeatStr(2, "str")).toEqual("strstr");
     });
-    test("when given 5, repeat it returns repeatrepeatrepeatrepeatrepeat", () => {
+    test("when given 5, repeat; it returns repeatrepeatrepeatrepeatrepeat", () => {
         expect(repeatStr(5, "repeat")).toEqual("repeatrepeatrepeatrepeatrepeat");
     });
-    test("when given 3, dash it returns dash", () => {
+    test("when given 3, dash; it returns dashdashdash", () => {
         expect(repeatStr(3, "dash")).toEqual("dashdashdash");
     });
 });
