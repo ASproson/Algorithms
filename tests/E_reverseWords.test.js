2579e711ffe9e9ba84af8dc59efeeac4d56b6048
atlas <a.sproson@outlook.com>
2023-08-04 23:03:09 +0100

    tsc build

---
+++ tests/E_reverseWords.test.js
@@ -0,0 +1,16 @@
+"use strict";
+const { reverseWords } = require("../Easy/E_reverseWords");
+describe("reverseWords()", () => {
+    test("when given The quick brown fox jumps over the lazy dog. expect ehT kciuq nworb xof spmuj revo eht yzal .god", () => {
+        expect(reverseWords("The quick brown fox jumps over the lazy dog.")).toEqual("ehT kciuq nworb xof spmuj revo eht yzal .god");
+    });
+    test("when given apple expect elppa", () => {
+        expect(reverseWords("apple")).toEqual("elppa");
+    });
+    test("when given a b c d expect a b c d", () => {
+        expect(reverseWords("a b c d")).toEqual("a b c d");
+    });
+    test("when given double  spaced  words expect elbuod  decaps  sdrow", () => {
+        expect(reverseWords("double  spaced  words")).toEqual("elbuod  decaps  sdrow");
+    });
+});
