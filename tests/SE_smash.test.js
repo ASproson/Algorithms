57407553557106241fce0681d87289f267ae3476
atlas <a.sproson@outlook.com>
2023-08-05 13:15:52 +0100

    tsc build

---
+++ tests/SE_smash.test.js
@@ -0,0 +1,15 @@
+"use strict";
+const { smash } = require("../SuperEasy/SE_smash");
+describe("smash", () => {
+    it("Should return empty string for empty array", () => {
+        expect(smash([])).toBe("");
+    });
+    it("One word example should return the word", () => {
+        expect(smash(["hello"])).toBe("hello");
+    });
+    it("Multiple words should be separated by spaces", () => {
+        expect(smash(["hello", "world"])).toBe("hello world");
+        expect(smash(["hello", "amazing", "world"])).toBe("hello amazing world");
+        expect(smash(["this", "is", "a", "really", "long", "sentence"])).toBe("this is a really long sentence");
+    });
+});
