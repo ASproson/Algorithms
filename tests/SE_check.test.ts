e652e6e0ad78712d2d7d60722104f63637541f5e
atlas <a.sproson@outlook.com>
2023-08-05 12:59:43 +0100

    Unit test

---
+++ tests/SE_check.test.ts
@@ -0,0 +1,16 @@
+const { check } = require("../SuperEasy/SE_check");
+
+describe("check()", () => {
+  test("when given [66, 101], 66 returns true", () => {
+    expect(check([66, 101], 66)).toEqual(true);
+  });
+  test("when given [80, 117, 115, 104, 45], 45 returns true", () => {
+    expect(check([80, 117, 115, 104, 45], 45)).toEqual(true);
+  });
+  test("when given ['t','e','s','t'], 'e' returns !", () => {
+    expect(check(["t", "e", "s", "t"], "e")).toEqual(true);
+  });
+  test("when given ['hello', 'world'], 'false' returns !", () => {
+    expect(check(["hello", "world"], "false")).toEqual(false);
+  });
+});
