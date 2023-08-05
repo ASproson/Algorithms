dbe74b27c696e9ce763f4594773f1e08c4cb795e
atlas <a.sproson@outlook.com>
2023-08-05 13:15:45 +0100

    tsc build

---
+++ SuperEasy/SE_smash.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.smash = void 0;
+const smash = (words) => {
+    return words.join(" ");
+};
+exports.smash = smash;
