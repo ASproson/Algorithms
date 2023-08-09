b1a85da3c54df83373ebe9942a8b574e063a4b36
atlas <a.sproson@outlook.com>
2023-08-09 08:12:00 +0100

    tsc build

--- SuperEasy/SE_mapping.js
+++ SuperEasy/SE_mapping.js
@@ -1,4 +1,5 @@
 "use strict";
+// Given an array of integers, return a new array with each value doubled
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mapping = void 0;
 const mapping = (arr) => {
