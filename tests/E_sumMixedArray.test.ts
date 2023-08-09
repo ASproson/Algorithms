9a6836a63b372952c8da05dc678405cc5185ebdb
atlas <a.sproson@outlook.com>
2023-08-09 08:04:44 +0100

    Unit test

---
+++ tests/E_sumMixedArray.test.ts
@@ -0,0 +1,13 @@
+import { sumMixedArray } from '../Easy/E_sumMixedArray';
+
+describe('sumMixedArray', () => {
+  test('when given [9, 3, "7", "3"] returns #', () => {
+    expect(sumMixedArray([9, 3, '7', '3'])).toEqual(22);
+  });
+  test('when given ["5", "0", 9 , 3, 2, 1, "9", 6, 7] returns 42', () => {
+    expect(sumMixedArray(['5', '0', 9, 3, 2, 1, '9', 6, 7])).toEqual(42);
+  });
+  test('when given ["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"] returns 41', () => {
+    expect(sumMixedArray(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0'])).toEqual(41);
+  });
+});
