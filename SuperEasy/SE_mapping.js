fa8bb3f2a1bf8ffceafbcff755e8ad4a849b70d0
atlas <a.sproson@outlook.com>
2023-08-09 08:11:24 +0100

    tsc build

---
+++ SuperEasy/SE_mapping.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mapping = void 0;
+const mapping = (arr) => {
+    return arr.map((num) => num * 2);
+};
+exports.mapping = mapping;
