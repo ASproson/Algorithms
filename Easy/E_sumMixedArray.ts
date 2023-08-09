acb940149ede4c8d6a5dce1f5a04f70114fa5690
atlas <a.sproson@outlook.com>
2023-08-09 08:05:25 +0100

    Adds problem comment

--- Easy/E_sumMixedArray.ts
+++ Easy/E_sumMixedArray.ts
@@ -1,3 +1,5 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers
+
 export const sumMixedArray = (arr: any[]): number => {
   return arr.reduce((a, b) => Number(a) + Number(b));
 };
