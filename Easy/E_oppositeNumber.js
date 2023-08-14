5b0444d675ba29db19f44615b5581c2e6c630cbc
atlas <a.sproson@outlook.com>
2023-08-14 08:21:24 +0100

    tsc buld

---
+++ Easy/E_oppositeNumber.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.oppositeNumber = void 0;
+const oppositeNumber = (n) => {
+    return -n;
+};
+exports.oppositeNumber = oppositeNumber;
