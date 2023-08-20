8f89f46b5e35319f5371388322e8b1c6f7991195
atlas <a.sproson@outlook.com>
2023-08-20 17:19:38 +0100

    tsc build

---
+++ Easy/E_findSmallestInt.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findSmallestInt = void 0;
+const findSmallestInt = (arr) => {
+    return Math.min(...arr);
+};
+exports.findSmallestInt = findSmallestInt;
