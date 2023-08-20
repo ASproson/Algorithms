5f3a001c068dfbfe687e91154341230edd87d8c4
atlas <a.sproson@outlook.com>
2023-08-20 17:14:21 +0100

    tsc build

---
+++ SuperEasy/SE_makeNegative.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.makeNegative = void 0;
+const makeNegative = (num) => {
+    if (num > 0)
+        return -num;
+    return num;
+};
+exports.makeNegative = makeNegative;
