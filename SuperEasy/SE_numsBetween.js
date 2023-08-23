bb7527dc853adb7f66c2fe5baf638056eeda6248
atlas <a.sproson@outlook.com>
2023-08-23 17:22:39 +0100

    tsc build

--- SuperEasy/SE_numsBetween.js
+++ SuperEasy/SE_numsBetween.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.numsBetween = void 0;
 const numsBetween = (a, b) => {
-    let numArr = [];
-    while (a <= b) {
-        numArr.push(a);
-    }
-    return numArr;
+    // let numArr = [];
+    // while (a <= b) {
+    //   numArr.push(a);
+    // }
+    // return numArr;
+    return Array.from({ length: b - a + 1 }, (_, idx) => a + idx);
 };
 exports.numsBetween = numsBetween;
