4c9e0073ee6081e61ac01f2c05eda8b54db22cb5
atlas <a.sproson@outlook.com>
2023-08-23 17:22:44 +0100

    Alt solution

--- SuperEasy/SE_numsBetween.ts
+++ SuperEasy/SE_numsBetween.ts
@@ -1,7 +1,8 @@
 export const numsBetween = (a: number, b: number): number[] => {
-  let numArr = [];
-  while (a <= b) {
-    numArr.push(a);
-  }
-  return numArr;
+  // let numArr = [];
+  // while (a <= b) {
+  //   numArr.push(a);
+  // }
+  // return numArr;
+  return Array.from({ length: b - a + 1 }, (_, idx) => a + idx);
 };
