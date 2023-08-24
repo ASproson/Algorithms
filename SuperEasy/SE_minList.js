d9c78d257151ece3f0b23245b7fcac2856b162eb
atlas <a.sproson@outlook.com>
2023-08-24 15:00:00 +0100

    tsc build

---
+++ SuperEasy/SE_minList.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.minList = void 0;
+const minList = (list) => {
+    return Math.min(...list);
+};
+exports.minList = minList;
