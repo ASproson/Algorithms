1c9d965a40231655b7a9ce5fefcd9b994fbd22fc
atlas <a.sproson@outlook.com>
2023-08-25 13:33:00 +0100

    Alternative solution using reduce

--- Easy/E_busStops.ts
+++ Easy/E_busStops.ts
@@ -2,19 +2,23 @@
 // arr[0] is current number on the bus, arr[1] is people leaving the bus
 // What is the total number of people on the bus at the end?
 
+// export const busStops = (stops: [number, number][]): number => {
+//   let on = 0;
+//   let off = 0;
+
+//   let flattened = stops.flat(Infinity);
+
+//   for (let i = 0; i < flattened.length; i++) {
+//     if (i % 2 === 0) {
+//       on += Number(flattened[i]);
+//     } else {
+//       off += Number(flattened[i]);
+//     }
+//   }
+
+//   return on - off;
+// };
+
 export const busStops = (stops: [number, number][]): number => {
-  let on = 0;
-  let off = 0;
-
-  let flattened = stops.flat(Infinity);
-
-  for (let i = 0; i < flattened.length; i++) {
-    if (i % 2 === 0) {
-      on += Number(flattened[i]);
-    } else {
-      off += Number(flattened[i]);
-    }
-  }
-
-  return on - off;
+  return stops.reduce((acc, [onBus, outBus]) => acc + onBus - outBus, 0);
 };
