97a882bf92d2909728278fa788615c13695c38d1
atlas <a.sproson@outlook.com>
2023-08-28 12:54:57 +0100

    tssc build

--- SuperEasy/SE_betterThanAverage.js
+++ SuperEasy/SE_betterThanAverage.js
@@ -1,9 +1,21 @@
 "use strict";
+// export const betterThanAverage = (
+//   classPoints: number[],
+//   yourPoints: number
+// ): boolean => {
+//   const totalPoints = classPoints.reduce((a, b) => a + b);
+//   const avg = totalPoints / classPoints.length;
+//   return yourPoints > avg ? true : false;
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.betterThanAverage = void 0;
 const betterThanAverage = (classPoints, yourPoints) => {
-    const totalPoints = classPoints.reduce((a, b) => a + b);
-    const avg = totalPoints / classPoints.length;
-    return yourPoints > avg ? true : false;
+    return yourPoints > getAverage(classPoints);
 };
 exports.betterThanAverage = betterThanAverage;
+const getSum = (array) => {
+    return array.reduce((a, b) => a + b);
+};
+const getAverage = (array) => {
+    return getSum(array) / array.length;
+};
