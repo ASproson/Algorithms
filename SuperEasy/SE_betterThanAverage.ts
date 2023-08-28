15e9926a2062cb72eb79219c758b42da1f294aa3
atlas <a.sproson@outlook.com>
2023-08-28 12:55:04 +0100

    Adds functional refactor

--- SuperEasy/SE_betterThanAverage.ts
+++ SuperEasy/SE_betterThanAverage.ts
@@ -1,8 +1,23 @@
+// export const betterThanAverage = (
+//   classPoints: number[],
+//   yourPoints: number
+// ): boolean => {
+//   const totalPoints = classPoints.reduce((a, b) => a + b);
+//   const avg = totalPoints / classPoints.length;
+//   return yourPoints > avg ? true : false;
+// };
+
 export const betterThanAverage = (
   classPoints: number[],
   yourPoints: number
 ): boolean => {
-  const totalPoints = classPoints.reduce((a, b) => a + b);
-  const avg = totalPoints / classPoints.length;
-  return yourPoints > avg ? true : false;
+  return yourPoints > getAverage(classPoints);
+};
+
+const getSum = (array: number[]): number => {
+  return array.reduce((a, b) => a + b);
+};
+
+const getAverage = (array: number[]): number => {
+  return getSum(array) / array.length;
 };
