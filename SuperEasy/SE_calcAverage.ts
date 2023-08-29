aacc2a39c10d28e7d375baf959b9ebd1367ba721
atlas <a.sproson@outlook.com>
2023-08-29 08:14:10 +0100

    Adds functional solution

--- SuperEasy/SE_calcAverage.ts
+++ SuperEasy/SE_calcAverage.ts
@@ -1,4 +1,16 @@
+// export const calcAverage = (arr: number[]): number => {
+//   if (arr.length === 0) return 0;
+//   return arr.reduce((a, b) => a + b) / arr.length;
+// };
+
+const sumArray = (arr: number[]): number => {
+  return arr.reduce((a, b) => a + b);
+};
+
+const getAverage = (arr: number[]): number => {
+  return sumArray(arr) / arr.length;
+};
+
 export const calcAverage = (arr: number[]): number => {
-  if (arr.length === 0) return 0;
-  return arr.reduce((a, b) => a + b) / arr.length;
+  return getAverage(arr);
 };
