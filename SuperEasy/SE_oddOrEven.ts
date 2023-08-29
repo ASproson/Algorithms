399a7297708007608857adf06b90dbb5c65fcaed
atlas <a.sproson@outlook.com>
2023-08-29 08:16:47 +0100

    Adds functional solution

--- SuperEasy/SE_oddOrEven.ts
+++ SuperEasy/SE_oddOrEven.ts
@@ -1,4 +1,12 @@
+// export const oddOrEven = (arr: number[]): string => {
+//   if (arr.length < 1) return 'even';
+//   return arr.reduce((a, b) => a + b) % 2 === 0 ? 'even' : 'odd';
+// };
+
+const sumArray = (arr: number[]): number => {
+  return arr.reduce((a, b) => a + b);
+};
+
 export const oddOrEven = (arr: number[]): string => {
-  if (arr.length < 1) return 'even';
-  return arr.reduce((a, b) => a + b) % 2 === 0 ? 'even' : 'odd';
+  return sumArray(arr) % 2 === 0 ? 'even' : 'odd';
 };
