47b23ca17d3b94eda8a194c51bcac72721d51eba
atlas <a.sproson@outlook.com>
2023-08-29 08:16:43 +0100

    tsc build

--- SuperEasy/SE_oddOrEven.js
+++ SuperEasy/SE_oddOrEven.js
@@ -1,9 +1,14 @@
 "use strict";
+// export const oddOrEven = (arr: number[]): string => {
+//   if (arr.length < 1) return 'even';
+//   return arr.reduce((a, b) => a + b) % 2 === 0 ? 'even' : 'odd';
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.oddOrEven = void 0;
+const sumArray = (arr) => {
+    return arr.reduce((a, b) => a + b);
+};
 const oddOrEven = (arr) => {
-    if (arr.length < 1)
-        return 'even';
-    return arr.reduce((a, b) => a + b) % 2 === 0 ? 'even' : 'odd';
+    return sumArray(arr) % 2 === 0 ? 'even' : 'odd';
 };
 exports.oddOrEven = oddOrEven;
