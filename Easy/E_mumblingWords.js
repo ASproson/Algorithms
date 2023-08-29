33a0bcd9af3d3039491f4f1986596c02e7edbf07
atlas <a.sproson@outlook.com>
2023-08-29 08:50:53 +0100

    tsc build

--- Easy/E_mumblingWords.js
+++ Easy/E_mumblingWords.js
@@ -1,21 +1,29 @@
 "use strict";
+// export const mumblingWords = (s: string): string => {
+//   let str = '';
+//   let count = 0;
+//   s.split('').forEach((e) => {
+//     count++;
+//     if (count === 1) {
+//       str += e;
+//     } else {
+//       let repeated = e.repeat(count).toLowerCase();
+//       let capital = repeated.charAt(0).toUpperCase();
+//       let remainingLetters = repeated.slice(1);
+//       str += '-' + capital + remainingLetters;
+//     }
+//   });
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mumblingWords = void 0;
+//   return str;
+// };
 const mumblingWords = (s) => {
-    let str = '';
-    let count = 0;
-    s.split('').forEach((e) => {
-        count++;
-        if (count === 1) {
-            str += e;
-        }
-        else {
-            let repeated = e.repeat(count).toLowerCase();
-            let capital = repeated.charAt(0).toUpperCase();
-            let remainingLetters = repeated.slice(1);
-            str += '-' + capital + remainingLetters;
-        }
-    });
-    return str;
+    const parts = [];
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i].toLowerCase();
+        const repeatedChar = char.repeat(i + 1);
+        parts.push(repeatedChar.charAt(0).toUpperCase() + repeatedChar.slice(1));
+    }
+    return parts.join('-');
 };
 exports.mumblingWords = mumblingWords;
