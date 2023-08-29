61943c39154db123bb515992aeeb6f560e16f81b
atlas <a.sproson@outlook.com>
2023-08-29 08:50:57 +0100

    Alt solution

--- Easy/E_mumblingWords.ts
+++ Easy/E_mumblingWords.ts
@@ -1,17 +1,29 @@
-export const mumblingWords = (s: string): string => {
-  let str = '';
-  let count = 0;
-  s.split('').forEach((e) => {
-    count++;
-    if (count === 1) {
-      str += e;
-    } else {
-      let repeated = e.repeat(count).toLowerCase();
-      let capital = repeated.charAt(0).toUpperCase();
-      let remainingLetters = repeated.slice(1);
-      str += '-' + capital + remainingLetters;
-    }
-  });
+// export const mumblingWords = (s: string): string => {
+//   let str = '';
+//   let count = 0;
+//   s.split('').forEach((e) => {
+//     count++;
+//     if (count === 1) {
+//       str += e;
+//     } else {
+//       let repeated = e.repeat(count).toLowerCase();
+//       let capital = repeated.charAt(0).toUpperCase();
+//       let remainingLetters = repeated.slice(1);
+//       str += '-' + capital + remainingLetters;
+//     }
+//   });
 
-  return str;
+//   return str;
+// };
+
+export const mumblingWords = (s: string): string => {
+  const parts = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i].toLowerCase();
+    const repeatedChar = char.repeat(i + 1);
+    parts.push(repeatedChar.charAt(0).toUpperCase() + repeatedChar.slice(1));
+  }
+
+  return parts.join('-');
 };
