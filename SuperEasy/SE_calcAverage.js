57a08ffa8cc3089875e95ff9631165e09fd66fd4
atlas <a.sproson@outlook.com>
2023-08-29 08:14:05 +0100

    tsc build

--- SuperEasy/SE_calcAverage.js
+++ SuperEasy/SE_calcAverage.js
@@ -1,9 +1,17 @@
 "use strict";
+// export const calcAverage = (arr: number[]): number => {
+//   if (arr.length === 0) return 0;
+//   return arr.reduce((a, b) => a + b) / arr.length;
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.calcAverage = void 0;
+const sumArray = (arr) => {
+    return arr.reduce((a, b) => a + b);
+};
+const getAverage = (arr) => {
+    return sumArray(arr) / arr.length;
+};
 const calcAverage = (arr) => {
-    if (arr.length === 0)
-        return 0;
-    return arr.reduce((a, b) => a + b) / arr.length;
+    return getAverage(arr);
 };
 exports.calcAverage = calcAverage;
