7fc4d51cf9f63e504e466cba12627209e1074737
atlas <a.sproson@outlook.com>
2023-08-31 09:29:56 +0100

    Alt solution

--- Easy/E_isUpperCase.ts
+++ Easy/E_isUpperCase.ts
@@ -1,8 +1,12 @@
+// export const isUpperCase = (str: string) => {
+//   return str
+//     .split('')
+//     .map((_, idx) => str.charCodeAt(idx) < 91)
+//     .includes(false)
+//     ? false
+//     : true;
+// };
+
 export const isUpperCase = (str: string) => {
-  return str
-    .split('')
-    .map((_, idx) => str.charCodeAt(idx) < 91)
-    .includes(false)
-    ? false
-    : true;
+  return str === str.toUpperCase();
 };
