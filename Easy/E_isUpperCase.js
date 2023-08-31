4081ec8656fec0efcede108b5c37fe8ef01d0ec8
atlas <a.sproson@outlook.com>
2023-08-31 09:30:03 +0100

    tsc build

--- Easy/E_isUpperCase.js
+++ Easy/E_isUpperCase.js
@@ -1,12 +1,15 @@
 "use strict";
+// export const isUpperCase = (str: string) => {
+//   return str
+//     .split('')
+//     .map((_, idx) => str.charCodeAt(idx) < 91)
+//     .includes(false)
+//     ? false
+//     : true;
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isUpperCase = void 0;
 const isUpperCase = (str) => {
-    return str
-        .split('')
-        .map((_, idx) => str.charCodeAt(idx) < 91)
-        .includes(false)
-        ? false
-        : true;
+    return str === str.toUpperCase();
 };
 exports.isUpperCase = isUpperCase;
