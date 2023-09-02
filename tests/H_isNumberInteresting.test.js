68cd0cf9690e4dd64dca88fb3e14f75a744c6c77
atlas <a.sproson@outlook.com>
2023-09-02 09:16:06 +0100

    Tsc build

---
+++ tests/H_isNumberInteresting.test.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const H_isNumberInteresting_1 = require("../Hard/H_isNumberInteresting");
+describe('solution', () => {
+    it('should work, dangit!', () => {
+        expect((0, H_isNumberInteresting_1.isNumberInteresting)(3, [1337, 256])).toBe(0);
+        expect((0, H_isNumberInteresting_1.isNumberInteresting)(1336, [1337, 256])).toBe(1);
+        expect((0, H_isNumberInteresting_1.isNumberInteresting)(1337, [1337, 256])).toBe(2);
+        expect((0, H_isNumberInteresting_1.isNumberInteresting)(11208, [1337, 256])).toBe(0);
+        expect((0, H_isNumberInteresting_1.isNumberInteresting)(11209, [1337, 256])).toBe(1);
+        expect((0, H_isNumberInteresting_1.isNumberInteresting)(11211, [1337, 256])).toBe(2);
+    });
+});
