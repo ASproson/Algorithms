4dd41eca72401f0bf8f42b40af02b442aacc7a90
atlas <a.sproson@outlook.com>
2023-09-02 09:16:01 +0100

    Part solution

---
+++ Hard/H_isNumberInteresting.ts
@@ -0,0 +1,41 @@
+const everyDigitIsTheSameNumber = (
+  numberString: string[],
+  firstNumber: string
+): number => {
+  return numberString.every((e) => e === firstNumber) ? 2 : 0;
+};
+
+const digitFollowedByAllZeroes = (numberString: string[]) => {
+  const numberStringCopy = [...numberString];
+  numberStringCopy.shift();
+  return numberStringCopy.every((e) => e === '0') ? 2 : 0;
+};
+
+const numbersAreSequential = (numberString: string[]): number => {
+  for (let i = 1; i < numberString.length; i++) {
+    let curr = Number(numberString[i]);
+    let prev = Number(numberString[i - 1]);
+    if (prev + 1 === curr || prev - 1 === curr) {
+      continue;
+    } else {
+      return 0;
+    }
+  }
+  return 2;
+};
+
+export const isNumberInteresting = (
+  n: number,
+  awesomePhrases: number[]
+): number => {
+  if (n <= 99) return 0;
+
+  const numberString = String(n).split('');
+  const firstNumber = numberString[0];
+
+  if (everyDigitIsTheSameNumber(numberString, firstNumber) === 2) return 2;
+  if (digitFollowedByAllZeroes(numberString) === 2) return 2;
+  if (numbersAreSequential(numberString) === 2) return 2;
+
+  return 0;
+};
