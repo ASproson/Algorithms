d0e4fe6a62fd3460b7b6ccef07e5ea325096c49b
atlas <a.sproson@outlook.com>
2023-09-02 09:17:21 +0100

    tsc build

--- Hard/H_isNumberInteresting.js
+++ Hard/H_isNumberInteresting.js
@@ -36,6 +36,7 @@ const isNumberInteresting = (n, awesomePhrases) => {
     return 0;
 };
 exports.isNumberInteresting = isNumberInteresting;
+// https://www.codewars.com/kata/52c4dd683bfd3b434c000292/train/typescript
 // "7777...8?!??!", exclaimed Bob, "I missed it again! Argh!" Every time there's an interesting number coming up, he notices and then promptly forgets. Who doesn't like catching those one-off interesting mileage numbers?
 // Let's make it so Bob never misses another interesting number. We've hacked into his car's computer, and we have a box hooked up that reads mileage numbers. We've got a box glued to his dash that lights up yellow or green depending on whether it receives a 1 or a 2 (respectively).
 // It's up to you, intrepid warrior, to glue the parts together. Write the function that parses the mileage number input, and returns a 2 if the number is "interesting" (see below), a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting.
