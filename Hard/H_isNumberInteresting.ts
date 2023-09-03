90e1fc99fd027f8f11f248b9e0464f503ca3039b
atlas <a.sproson@outlook.com>
2023-09-03 13:43:33 +0100

    Modifies comment

--- Hard/H_isNumberInteresting.ts
+++ Hard/H_isNumberInteresting.ts
@@ -115,15 +115,8 @@ export const isNumberInteresting = (
 
 // https://www.codewars.com/kata/52c4dd683bfd3b434c000292/train/typescript
 
-// "7777...8?!??!", exclaimed Bob, "I missed it again! Argh!" Every time there's an interesting number coming up, he notices and then promptly forgets. Who doesn't like catching those one-off interesting mileage numbers?
+// Returns a 2 if the number is "interesting" (see below), a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting
 
-// Let's make it so Bob never misses another interesting number. We've hacked into his car's computer, and we have a box hooked up that reads mileage numbers. We've got a box glued to his dash that lights up yellow or green depending on whether it receives a 1 or a 2 (respectively).
-
-// It's up to you, intrepid warrior, to glue the parts together. Write the function that parses the mileage number input, and returns a 2 if the number is "interesting" (see below), a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting.
-
-// Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
-
-// "Interesting" Numbers
 // Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
 
 // Any digit followed by all zeros: 100, 90000
@@ -152,8 +145,9 @@ export const isNumberInteresting = (
 // isInteresting(1335, [1337, 256]); // 1
 // isInteresting(1336, [1337, 256]); // 1
 // isInteresting(1337, [1337, 256]); // 2
+
 // Error Checking
 // A number is only interesting if it is greater than 99!
 // Input will always be an integer greater than 0, and less than 1,000,000,000.
 // The awesomePhrases array will always be provided, and will always be an array, but may be empty. (Not everyone thinks numbers spell funny words...)
-// You should only ever output 0, 1, or 2.
+// You should only ever output 0, 1, or 2
