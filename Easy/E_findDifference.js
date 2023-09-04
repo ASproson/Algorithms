160a3d99a7fe8612fc50f94e1500f3e72bb8da96
atlas <a.sproson@outlook.com>
2023-09-04 08:24:02 +0100

    tsc build

--- Easy/E_findDifference.js
+++ Easy/E_findDifference.js
@@ -3,9 +3,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.findDifference = void 0;
 // For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8
+// export const findDifference = (
+//   a: [number, number, number],
+//   b: [number, number, number]
+// ): number => {
+//   const aSum = a.reduce((a, b) => a * b);
+//   const bSum = b.reduce((a, b) => a * b);
+//   return aSum > bSum ? aSum - bSum : bSum - aSum;
+// };
 const findDifference = (a, b) => {
-    const aSum = a.reduce((a, b) => a * b);
-    const bSum = b.reduce((a, b) => a * b);
-    return aSum > bSum ? aSum - bSum : bSum - aSum;
+    return Math.abs(a.reduce((a, b) => a * b) - b.reduce((a, b) => a * b));
 };
 exports.findDifference = findDifference;
