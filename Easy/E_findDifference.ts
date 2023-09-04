0a4f48a643b2cc730fb887424e39faa17c1f68ec
atlas <a.sproson@outlook.com>
2023-09-04 08:24:09 +0100

    Reduces to one liner

--- Easy/E_findDifference.ts
+++ Easy/E_findDifference.ts
@@ -2,11 +2,18 @@
 
 // For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8
 
+// export const findDifference = (
+//   a: [number, number, number],
+//   b: [number, number, number]
+// ): number => {
+//   const aSum = a.reduce((a, b) => a * b);
+//   const bSum = b.reduce((a, b) => a * b);
+//   return aSum > bSum ? aSum - bSum : bSum - aSum;
+// };
+
 export const findDifference = (
   a: [number, number, number],
   b: [number, number, number]
 ): number => {
-  const aSum = a.reduce((a, b) => a * b);
-  const bSum = b.reduce((a, b) => a * b);
-  return aSum > bSum ? aSum - bSum : bSum - aSum;
+  return Math.abs(a.reduce((a, b) => a * b) - b.reduce((a, b) => a * b));
 };
