939d363e17637d3e5f506d7a0f2bb63315ee0a0a
atlas <a.sproson@outlook.com>
2023-09-04 08:06:47 +0100

    tsc build

---
+++ SuperEasy/SE_strToArray.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.strToArray = void 0;
+const strToArray = (s) => {
+    return s.split(' ');
+};
+exports.strToArray = strToArray;
