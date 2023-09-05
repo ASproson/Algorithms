0a0a3f86d74484591a031f263718a41ff0090fec
atlas <a.sproson@outlook.com>
2023-09-05 14:22:29 +0100

    Solution

---
+++ SuperEasy/SE_numToString.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.numToString = void 0;
+const numToString = (num) => {
+    return String(num);
+};
+exports.numToString = numToString;
