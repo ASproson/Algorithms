adcd59f92ff45d8ddd818241f3a8dc874f245ef8
atlas <a.sproson@outlook.com>
2023-09-12 08:19:02 +0100

    tsc build

---
+++ SuperEasy/SE_squareNum.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.squareNum = void 0;
+const squareNum = (num) => {
+    return num ** 2;
+};
+exports.squareNum = squareNum;
