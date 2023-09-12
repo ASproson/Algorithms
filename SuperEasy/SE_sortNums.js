0cc5a59704535e24dea7aa78e4a05364bd43b65a
atlas <a.sproson@outlook.com>
2023-09-12 08:14:09 +0100

    tsc build

---
+++ SuperEasy/SE_sortNums.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sortNums = void 0;
+const sortNums = (nums) => {
+    return nums.sort((a, b) => a - b);
+};
+exports.sortNums = sortNums;
