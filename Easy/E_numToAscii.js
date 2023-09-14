bf5ee0c3db3aeeadea150370bcbd1f8f8f5903f9
atlas <a.sproson@outlook.com>
2023-09-14 08:23:50 +0100

    tsc build

---
+++ Easy/E_numToAscii.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.numToAscii = void 0;
+const numToAscii = (c) => {
+    return String.fromCharCode(c);
+};
+exports.numToAscii = numToAscii;
