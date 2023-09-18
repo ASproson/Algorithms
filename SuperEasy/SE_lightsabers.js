797cf8bff43bf313ebc29a1f48866c35ec7fd472
atlas <a.sproson@outlook.com>
2023-09-18 08:17:33 +0100

    tsc build

---
+++ SuperEasy/SE_lightsabers.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.howManyLightSabers = void 0;
+const howManyLightSabers = (name) => {
+    if (name === 'Zach')
+        return 18;
+    return 0;
+};
+exports.howManyLightSabers = howManyLightSabers;
