88f67abc1732df5e9cd71724291ac62cf16477ab
atlas <a.sproson@outlook.com>
2023-09-18 08:17:45 +0100

    solution

---
+++ tests/SE_lightsabers.test.ts
@@ -0,0 +1,14 @@
+import { howManyLightSabers } from '../SuperEasy/SE_lightsabers';
+
+describe('howManyLightSabers()', () => {
+  test('returns 18 when given Zach', () => {
+    expect(howManyLightSabers('Zach')).toEqual(18);
+  });
+  test('returns 0 when given any name other than Zach', () => {
+    expect(howManyLightSabers('Adam')).toEqual(0);
+  });
+
+  test('returns 0 when given any name other than Zach', () => {
+    expect(howManyLightSabers()).toEqual(0);
+  });
+});
